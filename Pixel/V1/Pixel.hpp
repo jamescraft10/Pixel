53243062c6941828c093a6cfe1875de62c1ae719
JamesCraft10 <69867626+James251133@users.noreply.github.com>
2023-09-19 19:59:57 -0400

    Add files via upload

--- Pixel/V1/Pixel.hpp
+++ Pixel/V1/Pixel.hpp
@@ -1,9 +1,45 @@
 #include <iostream>
 #include <string>
-#include "Names.hpp"
+#include <cstdlib>
 
 namespace Pixel {
-    void Pixel(std::string hexCode) {
-        std::cout << hexCode;
+    void Print(std::string Value) {
+        std::cout << Value;
+    }
+
+    void BackgroundColor(int R, int G, int B) {
+        std::cout << "\033[48;2;" << R << ";" << G << ";" << B << "m";
+    }
+
+    void TextColor(int R, int G, int B) {
+        std::cout << "\033[38;2;" << R << ";" << G << ";" << B << "m";
+    }
+
+    void ResetColor() {
+        std::cout << "\033[0m";
+    }
+
+    void TurnOffFormatting() {
+        std::cout << "\033[0m";
+    }
+
+    void TurnOnUnderline() {
+        std::cout << "\033[4m";
+    }
+
+    void TurnOffUnderline() {
+        std::cout << "\033[24m";
+    }
+
+    void TurnOnBold() {
+        std::cout << "\033[1m";
+    }
+
+    void TurnOffBold() {
+        std::cout << "\033[22m";
+    }
+
+    void ClearScrean() {
+        std::system("clear");
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,37 +1,17 @@
-//#include "Pixel/V1/Pixel.hpp"
-#include <iostream>
+#include "Pixel/V1/Pixel.hpp"
 
 int main() {
-    std::cout<<"\033[38;2;26;99;217m\033[48;2;55;181;33mHello Colorful World!";
-    std::cout<<"\033[38;2;0;0;0m\033[48;2;0;0;0m";
-
-    // \033[38;2;<r>;<g>;<b>m // RGB text color
-    // \033[48;2;<r>;<g>;<b>m // RGB background color
-    // \033[39m\\033[49m // RESET
-    // \\033[<L>;<C>H or \\033[<L>;<C>f
-
-    /*
-        #!/usr/bin/env python3
-
-        for i in range(30, 37 + 1):
-            print("\033[%dm%d\t\t\033[%dm%d" % (i, i, i + 60, i + 60))
-
-        print("\033[39m\\033[49m                 - Reset color")
-        print("\\033[2K                          - Clear Line")
-        print("\\033[<L>;<C>H or \\033[<L>;<C>f  - Put the cursor at line L and column C.")
-        print("\\033[<N>A                        - Move the cursor up N lines")
-        print("\\033[<N>B                        - Move the cursor down N lines")
-        print("\\033[<N>C                        - Move the cursor forward N columns")
-        print("\\033[<N>D                        - Move the cursor backward N columns\n")
-        print("\\033[2J                          - Clear the screen, move to (0,0)")
-        print("\\033[K                           - Erase to end of line")
-        print("\\033[s                           - Save cursor position")
-        print("\\033[u                           - Restore cursor position\n")
-        print("\\033[4m                          - Underline on")
-        print("\\033[24m                         - Underline off\n")
-        print("\\033[1m                          - Bold on")
-        print("\\033[21m                         - Bold off")
-    */
-
+    Pixel::ClearScrean();
+    Pixel::TurnOnBold();
+    Pixel::TurnOnUnderline();
+    Pixel::BackgroundColor(255,255,255);
+    Pixel::TextColor(0,0,0);
+    Pixel::Print("Hello, World!\n");
+    Pixel::ResetColor();
+    Pixel::TurnOffFormatting();
+    Pixel::BackgroundColor(0,0,0);
+    Pixel::TextColor(255,255,255);
+    Pixel::Print("Goodbye, World!");
+    Pixel::ResetColor();
     return 0;
 }
